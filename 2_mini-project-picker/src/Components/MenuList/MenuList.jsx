7f427dd17017835d8c4c6a20f369d663a1c12e8f
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-05-06 23:07:34 +0700

    Commit 10 - Mini Projet Picker : partie 3 [Remplacer les doublons du MenuList : utiliser .map()]

--- 2_mini-project-picker/src/Components/MenuList/MenuList.jsx
+++ 2_mini-project-picker/src/Components/MenuList/MenuList.jsx
@@ -1,29 +1,17 @@
 import s from "./style.module.css";
 import { MenuListItem } from "../MenuListItem/MenuListItem";
+import { DIFFICULTIES } from "./constant";
 
 export function MenuList({ onItemClick, difficulty }) {
   return (
     <div className={s.container}>
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Low"
-        isSelected={difficulty === "Low"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Medium"
-        isSelected={difficulty === "Medium"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="High"
-        isSelected={difficulty === "High"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Insane"
-        isSelected={difficulty === "Insane"}
-      />
+      {DIFFICULTIES.map((diff) => (
+        <MenuListItem
+          onClick={onItemClick}
+          difficulty={diff}
+          isSelected={difficulty === diff}
+        />
+      ))}
     </div>
   );
 }
